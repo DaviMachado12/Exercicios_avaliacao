a0c7ca9af41ab4cc7d412d69ac532142e441c44e
Davi <85639277+SagattZord@users.noreply.github.com>
2022-09-22 19:40:45 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TABUADA DO 1 AO 10\n");
+            for(int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.WriteLine(x + " x " + y + " = " + x * y);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
